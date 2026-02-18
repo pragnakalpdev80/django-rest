3bbf0e988b8a337bbe3ffb21bd9bc8b64598dd0a
Hill Soni Dev80 <pragnakalp.dev80@gmail.com>
2026-02-18 17:21:54 +0530

    level-1:exercise-4

--- level-1/api/views.py
+++ level-1/api/views.py
@@ -30,17 +30,18 @@ class TaskViewSet(viewsets.ModelViewSet):
             'data': serializer.data
         }, status=status.HTTP_201_CREATED)
     
-    # def update(self, request, *args, **kwargs):
-    #     serializer = self.get_serializer(data=request.data)
-    #     serializer.is_valid(raise_exception=True)
-    #     serializer.save()
-    #     print(serializer.data)
-    #     # Custom response
-    #     return Response({
-    #         'success': True,
-    #         'message': 'Task updated successfully',
-    #         'data': serializer.data
-    #     }, status=status.HTTP_200_OK)
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        return Response({
+            'success': True,
+            'message': 'Task updated successfully',
+            'data': serializer.data
+        }, status=status.HTTP_200_OK)
     
     def list(self, request, *args, **kwargs):
         queryset = self.filter_queryset(self.get_queryset())
