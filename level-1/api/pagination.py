5bd6f974a5582341ecb7df63214b6815ddf81ea5
Hill Soni Dev80 <pragnakalp.dev80@gmail.com>
2026-02-20 15:59:11 +0530

    level 2b : exercise 4

--- level-1/api/pagination.py
+++ level-1/api/pagination.py
@@ -1,6 +1,7 @@
 from rest_framework.pagination import LimitOffsetPagination
 
 class BookLimitOffsetPagination(LimitOffsetPagination):
+    
     default_limit = 20
     limit_query_param = 'limit'
     offset_query_param = 'offset'
